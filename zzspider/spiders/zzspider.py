8f033e72781304a6ad49e6475fda0d0b58d6fd0e
Ins <mcshr@hotmail.com>
2022-01-01 07:14:10 +0000

    update zzspider/spiders/zzspider.py.

--- zzspider/spiders/zzspider.py
+++ zzspider/spiders/zzspider.py
@@ -151,8 +151,8 @@ class zzspider(scrapy.Spider):
                     new_file.write(file + b'\0')
             os.remove(temp_path)
 
-            linux_relate_path = f"/zb_users/upload/{str(now.year)}/{str(now.month)}"
-            sftp.upload_to_dir(real_path, ConfigUtil.config['sftp']['path'] + linux_relate_path)
+            #linux_relate_path = f"/zb_users/upload/{str(now.year)}/{str(now.month)}"
+            #sftp.upload_to_dir(real_path, ConfigUtil.config['sftp']['path'] + linux_relate_path)
             self.insert_upload(real_path)
             upload_count += 1
             content_str = content_str.replace(src, linux_relate_path + f"/{filename}")
