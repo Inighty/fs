3cbe1fdad88dabff41517a07ea010287e278d9d2
Inighty <mcshr@hotmail.com>
2022-02-24 09:44:10 +0800

    test

---
+++ baidu_relate.py
@@ -0,0 +1,11 @@
+import sys
+
+from baiduspider import BaiduSpider
+
+arg = sys.argv
+if len(arg) > 1:
+    start_word = arg[1]
+    baiduspider = BaiduSpider()
+    result_all = baiduspider.search_web(start_word, 1,
+                                        ['news', 'video', 'baike', 'tieba', 'blog', 'gitee', 'calc', 'music'])
+    print("相关搜索数量：" + str(len(result_all.related)))
