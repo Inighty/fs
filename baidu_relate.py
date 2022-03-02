0ba59d26a60463e1b6759cef9c8484e208b949f2
Ins <mcshr@hotmail.com>
2022-03-02 20:55:31 +0800

    up

--- baidu_relate.py
+++ baidu_relate.py
@@ -8,4 +8,6 @@ if len(arg) > 1:
     baiduspider = BaiduSpider()
     result_all = baiduspider.search_web(start_word, 1,
                                         ['news', 'video', 'baike', 'tieba', 'blog', 'gitee', 'calc', 'music'])
+    for item in result_all.related:
+        print(item)
     print("相关搜索数量：" + str(len(result_all.related)))
