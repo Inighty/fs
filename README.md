304d6cb05ede37d497924dee35617455e56108ab
Inighty <mcshr@hotmail.com>
2022-03-11 11:39:34 +0800

    cd

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-yum -y install https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-99.0.4844.51-1-1.x86_64.rpm
+yum -y install https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-62.0.3202.62-1-1.x86_64.rpm
+
+https://chromedriver.storage.googleapis.com/index.html
+
+##查询版本
+https://www.ubuntuupdates.org/package/google_chrome/stable/main/base/google-chrome-stable
--- zzspider/tools/browser.py
+++ zzspider/tools/browser.py
@@ -4,14 +4,12 @@ import logging
 import os
 import time
 
-from selenium import webdriver
-from selenium.webdriver import DesiredCapabilities
-from selenium.webdriver.chrome.options import Options
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from undetected_chromedriver import ChromeOptions
 
-from zzspider import settings
 from zzspider.tools.singleton_type import Singleton
 
 logger = logging.getLogger(__name__)
@@ -20,49 +18,17 @@ basedir = os.path.dirname(os.path.realpath('__file__'))
 
 
 class Browser(metaclass=Singleton):
+
     # 构造函数
     def __init__(self):
-        # instantiate a chrome options object so you can set the size and headless preference
-        self.chrome_options = Options()
-        self.chrome_options.add_argument("--window-size=1920x1080")
         self.driver = None
-        # comment out the following line if you don't want to actually show Chrome instance
-        # but you can still see that the crawling is working via output in console
-
-        self.chrome_options.add_argument("--headless")
-        self.chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
-        self.chrome_options.add_argument('--start-maximized')
-        self.chrome_options.add_argument('--no-sandbox')
-        self.chrome_options.add_argument("--disable-extensions")
-        self.chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-        self.chrome_options.add_argument('--ignore-certificate-errors-spki-list')  # 屏蔽ssl error
-        self.chrome_options.add_argument('-ignore -ssl-errors')  # 屏蔽ssl error
-        No_Image_loading = {"profile.managed_default_content_settings.images": 2}
-        self.chrome_options.add_experimental_option("prefs", No_Image_loading)
-        self.chrome_options.add_argument('--disable-dev-shm-usage')
-        # self.chrome_options.add_argument(f'user-agent={settings.USER_AGENT}')
-        # comment out the following two lines to setup ProxyMesh service
-        # make sure you add the IP of the machine running this script to you ProxyMesh account for IP authentication
-        # IP:PORT or HOST:PORT you get this in your account once you pay for a plan
-
-        # PROXY = "114.115.220.11:8095"
-        # self.chrome_options.add_argument('--proxy-server=%s' % PROXY)
 
     def start_driver(self):
         if self.driver is not None:
             return self.driver
-        caps = DesiredCapabilities.CHROME
-        caps['acceptSslCerts'] = False  # 屏蔽ssl error
-        self.driver = webdriver.Chrome(chrome_options=self.chrome_options, desired_capabilities=caps)
-
-        self.driver.execute_cdp_cmd('Network.setUserAgentOverride', {
-            "userAgent": f'{settings.USER_AGENT}'})
-        with open(os.path.dirname(os.path.abspath(__file__)) + '/stealth.min.js') as f:
-            js = f.read()
-            self.driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-                "source": js
-            })
-        self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        options = ChromeOptions()
+        options.ignore_local_proxy_environment_variables()
+        self.driver = uc.Chrome(options=options)
         return self.driver
 
     # 关闭标签
