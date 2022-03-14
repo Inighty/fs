5c7f058733f441922971eb8a2a65f9f0d272f0d7
Inighty <mcshr@hotmail.com>
2022-03-14 14:24:24 +0800

    cd

--- zzspider/tools/img.py
+++ zzspider/tools/img.py
@@ -8,11 +8,24 @@ def img_to_progressive(path):
         return
     if os.path.isdir(path):
         return
+    destination = os.path.splitext(path)[0] + '_destination.' + os.path.splitext(path)[1]
     img_size = int(os.path.getsize(path))
+    if os.path.isfile(path) and os.path.splitext(path)[1] == '.png':
+        cmd = 'pngquant --force --skip-if-larger --output {} --quality 50-80 --verbose {}'.format(destination, path)
+        rt = os.system(cmd)
+        if rt == 0:
+            print(path.split('/')[-1:][0], '转换完毕')
+            print('开始重命名文件')
+            os.remove(path)
+            os.rename(destination, path)
+            new_img_size = int(os.path.getsize(path))
+            print('图片大小：' + str(img_size) + '--->' + str(new_img_size))
+            return
+
     if img_size < 500000:
         return
     img = Image.open(path)
-    destination = path.split('.')[:-1][0] + '_destination.' + path.split('.')[-1:][0]
+
     if img.mode == "CMYK":
         img = img.convert('RGB')
     new_width = 600
