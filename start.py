7313dd17c8cd77ac6ae6d27e6e214ec8cf4d10ef
Inighty <mcshr@hotmail.com>
2022-03-15 14:33:37 +0800

    cd

--- start.py
+++ start.py
@@ -52,13 +52,15 @@ def baidu_relate(start_word, relate_arr):
     time_num = 0
     try:
         while len(relate_arr) == 0 and time_num < 100:
+            proxy_ip = {
+                "http": "http://" + proxy_util.get(),  # HTTP代理
+                "https": "http://" + proxy_util.get()  # HTTPS代理
+            }
             logger.error("baidu relate time:" + str(time_num))
             result_all = baiduspider.search_web(start_word, 1,
                                                 ['news', 'video', 'baike', 'tieba', 'blog', 'gitee', 'calc', 'music'],
-                                                proxies={
-                                                    "http": "http://" + proxy_util.get(),
-                                                    "https": "http://" + proxy_util.get()
-                                                })
+                                                proxies=proxy_ip)
+            logger.error("baidu relate search done:")
             if len(result_all.related) > 0:
                 relate_arr.extend(result_all.related)
             else:
