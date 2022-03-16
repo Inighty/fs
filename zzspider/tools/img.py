6eb7f3a8bbf1b96ade84ea173aba56360d6baee9
Inighty <mcshr@hotmail.com>
2022-03-16 17:18:05 +0800

    thread

--- zzspider/tools/img.py
+++ zzspider/tools/img.py
@@ -33,7 +33,7 @@ def img_to_progressive(path):
         new_height = int(new_width * img.size[1] * 1.0 / img.size[0])
         img = img.resize((new_width, new_height))
     print(path.split('/')[-1:][0], '开始转换图片')
-    img.save(destination, "PNG", quality=80, optimize=True, progressive=True)
+    img.save(destination, "PNG", quality=60, optimize=True, progressive=True)
     print(path.split('/')[-1:][0], '转换完毕')
     print('开始重命名文件')
     os.remove(path)
